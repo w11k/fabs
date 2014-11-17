ccee132f4d988c7e3790bc349d5bc0b3036ad22e
Philipp Burgmer <burgmer@weiglewilczek.com>
2014-11-17 12:34:09 +0100

    chore(karma): remove outdated comments

--- src/snippets/karma-e2e.tpl.js
+++ src/snippets/karma-e2e.tpl.js
@@ -2,14 +2,8 @@
 
 module.exports = function (karmaConfig) {
   karmaConfig.set({
-    /** 
-     * From where to look for files, starting with the location of this file.
-     */
     basePath: '../',
 
-    /**
-     * This is the list of file patterns to load into the browser during testing.
-     */
     files: [
       <%= scripts.map(function(file) { return '\'' + file + '\'' }).join(',\n      ') %>
     ],
@@ -29,10 +23,6 @@ module.exports = function (karmaConfig) {
       '/': 'http://localhost:<%= connect_e2e_port %>/'
     },
 
-
-    /**
-     * How to report, by default.
-     */
     reporters: [
       'progress'
     ],
@@ -41,30 +31,11 @@ module.exports = function (karmaConfig) {
       outputFile: '<%= junit_results %>'
     },
 
-    /**
-     * On which port should the browser connect and what is the URL path for the browser to use.
-     */
     port: <%= port %>,
     urlRoot: '/tests/e2e/',
 
-    /**
-     * Disable file watching by default.
-     */
     autoWatch: false,
 
-    /**
-     * The list of browsers to launch to test on. This includes only "Firefox" by
-     * default, but other browser names include:
-     * Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
-     *
-     * Note that you can also use the executable name of the browser, like "chromium"
-     * or "firefox", but that these vary based on your operating system.
-     *
-     * You may also leave this blank and manually navigate your browser to
-     * http://localhost:9018/ when you're running tests. The window/tab can be left
-     * open and the tests will automatically occur there during the build. This has
-     * the aesthetic advantage of not launching a browser every time you save.
-     */
     browsers: [
       <%= browsers.map(function(browser) { return '\'' + browser + '\'' }).join(',\n      ') %>
     ]
