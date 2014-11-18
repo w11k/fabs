31b4a0c0de27ffd204042497ad444e61a6427034
Philipp Burgmer <burgmer@weiglewilczek.com>
2014-11-18 13:07:28 +0100

    chore(grunt): improve changelog generation and commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,64 @@
 
 var grunt = require('grunt');
 
+function getVersion() {
+  var pkg = grunt.file.readJSON('./package.json');
+  return pkg.version;
+}
+
 module.exports = function () {
   grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.loadNpmTasks('grunt-git');
 
   grunt.registerTask('release', [
-    'bump',
-    'changelog'
+    'bump:minor',
+    'changelog',
+    'gitadd:changelog',
+    'gitcommit:changelog'
+  ]);
+
+  grunt.registerTask('hotfix', [
+    'bump:patch',
+    'changelog',
+    'gitadd:changelog',
+    'gitcommit:changelog'
   ]);
 
   grunt.initConfig({
+    lazy: {
+      getVersion: getVersion
+    },
     bump: {
       options: {
         files: ['package.json'],
         commit: true,
         commitMessage: 'chore(project): bump version to %VERSION%',
-        commitFiles: ['package.json'], // '-a' for all files
+        commitFiles: ['package.json'],
         createTag: false,
         push: false
+      }
+    },
+    changelog: {
+      options: {
+        editor: 'open -W'
+      }
+    },
+    gitadd: {
+      changelog: {
+        files: {
+          src: ['Changelog.md']
+        }
+      }
+    },
+    gitcommit: {
+      changelog: {
+        options: {
+          message: 'doc(changelog): update changelog for <%= lazy.getVersion() %>'
+        },
+        files: {
+          src: ['Changelog.md']
+        }
       }
     }
   });
